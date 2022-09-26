024fd5d0815eb4d8571f1f98336708dc9774873f
erwanCdc <erwan.cadic@outlook.fr>
2022-09-26 17:24:32 +0200

    commit

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 #REQUIRES
 - npm install express --save
 - npm install seedrandom --save
+- npm install path --save
+- npm install body-parser --save
--- index.js
+++ index.js
@@ -1,50 +1,56 @@
-const express = require('express');
-const seedrandom = require('seedrandom');
+const express = require('express')
+const seedrandom = require('seedrandom')
+const bodyParser = require("body-parser");
 const path = require('path')
 const os = require('os')
-const fs = require('fs');
+const fs = require('fs')
 
-const app = express();
+const app = express()
 app.use(express.static(path.join(__dirname, 'www')))
 const port = process.env.PORT || 3000
 
-var currentWord = null;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// Read & store words into an array
+var words = fs.readFileSync('./www/data/liste_francais_utf8.txt').toString().split("\r\n")
+console.log('List Initialized')
+
+// Seed generator
+var today = new Date()
+var day = today.getDate()
+var month = today.getMonth()+1
+var year = today.getFullYear()
+const daySeed = day*3 + month*2 + year
+const generator = seedrandom(daySeed)
+const randomNumber = Math.floor(generator() * words.length)
+currentWord = words[randomNumber]
+console.log('Word generated : ' + words[randomNumber])
 
 app.get('/', (req,res) => {
 
-	if (currentWord === null){
-		// Read & store words into an array
-		var words = fs.readFileSync('./www/data/liste_francais_utf8.txt').toString().split("\r\n");
-		console.log('List Initialized');
+})
 
-		// Seed generator
-		var today = new Date();
-		var day = today.getDate();
-		var month = today.getMonth()+1;
-		var year = today.getFullYear();
-		const daySeed = day*3 + month*2 + year;
-		const generator = seedrandom(daySeed);
-		const randomNumber = Math.floor(generator() * words.length);
-		currentWord = words[randomNumber];
-		console.log('Word generated : ' + words[randomNumber]);
-	}
-});
+app.post('/', (req, res) => {
+	console.log(req.body);
+})
 
 
 app.get('/mot', (req, res) => {
-	res.send(currentWord);
-});
+	res.send(currentWord)
+})
 
 
 app.get('/port', (req,res) => {
-	res.send("MOTUS is listening on " + os.hostname() + " port:  " + port);
-});
+	res.send("MOTUS is listening on " + os.hostname() + " port:  " + port)
+})
 
 
 
 app.listen(port, () => {
 	console.log(`Application running on port ${port}`)
-});
+})
 
 
 
