6fa2b988369bcbf45ce55d3010c7d26411c529e4
Naditus <lucas.chapron@gmail.com>
2022-09-30 16:43:55 +0200

    show upper text in app + test compare with list

--- www/js/wordSubmitController.js
+++ www/js/wordSubmitController.js
@@ -49,7 +49,7 @@ $(document).ready(function(){
             if(i==0){
                 if(j==0){
                     cell.setAttribute('id', ('col'+j))
-                    var cellText = document.createTextNode(target[j])
+                    var cellText = document.createTextNode(target[j].toUpperCase())
                 }
                 else{
                     cell.setAttribute('id', ('col'+j))
@@ -78,7 +78,7 @@ $(document).ready(function(){
         let k = 0
 
         boxes.forEach((node) => {
-           node.innerHTML = submit[k]
+           node.innerHTML = submit[k].toUpperCase()
            let color = 'green'
            
            if(comp[k]=='?'){
@@ -95,7 +95,7 @@ $(document).ready(function(){
 
         iterator = iterator + 1
 
-        if (comp == submit) win = true
+        if (comp.toLowerCase() == submit.toLowerCase()) win = true
 
         
     }
@@ -103,7 +103,8 @@ $(document).ready(function(){
     function word_comparison(submit){
     
         var diff = ""
-        target.toLowerCase()
+        target = target.toLowerCase()
+        submit = submit.toLowerCase()
 
        for(i=0; i<target.length; i++) {
             if (target[i] == submit[i]){
@@ -121,19 +122,44 @@ $(document).ready(function(){
         return diff
     }
 
+    // first test to see if the word is in the list
+
+    // function isInWords(submit_word){
+        
+    //     var dict = false
+    //     var {words} = require('../../index.js')
+    //     if (words.includes(submit_word.toLowerCase())) {
+    //         dict = false
+    //     } else {
+    //         dict = true
+    //     }
+
+    //     return dict
+    // }
+
 
     $('#gamePlace').submit(function( event ) {
-        if (!win){
-            var submit = $('#testWord').val().normalize('NFD').replace(/[\u0300-\u036f]/g, '')
-            update_table(submit)
-            $('#testWord').val('')
-            if (win == true){
-                alert('YOU WIN')
-                sessionStorage.setItem('score', (parseInt(sessionStorage.getItem('score')) + 1))
-                console.log("score = " + sessionStorage.getItem('score'))
-                location.reload()
+
+        var submit = $('#testWord').val().normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+
+        // first test to see if the word is in the list
+        // var dic = isInWords(submit)
+        // var {words} = require('../../index.js')
+        // if (words.includes(submit_word.toLowerCase())) {
+            // alert("Le mot " + submit.toUpperCase() + " n'est pas dans le dico !")
+        // } else {
+            if (!win){
+                update_table(submit)
+                $('#testWord').val('')
+                if (win == true){
+                    alert('YOU WIN')
+                    sessionStorage.setItem('score', (parseInt(sessionStorage.getItem('score')) + 1))
+                    console.log("score = " + sessionStorage.getItem('score'))
+                    // location.reload()
+                }
             }
-        }
+        // }
+
 
         return false
     })
