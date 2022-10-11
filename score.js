b826cd06990170b958bea44a801ec80679c24a00
alexisjapas <alexis.japas.dev@pm.me>
2022-10-11 21:10:38 +0200

    score.js genesis

---
+++ score.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const os = require('os')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = process.env.PORT || 3001
+const host = 'localhost:3001'
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.text())
+
+var score = 0
+var username
+var password
+
+
+app.get('/get_score', (req, res) => {
+	// Retrieves user id
+	//user = req.body.id
+
+	// Get score from
+	console.log('Score: ', score)
+	
+	result = {score:score}
+	console.log(result)
+
+	result = JSON.stringify(result)
+	console.log(result)
+
+	res.send(result)
+})
+
+
+app.listen(port, () => {
+	console.log('Application running on ' + os.hostname() + " port : " + port)
+})
