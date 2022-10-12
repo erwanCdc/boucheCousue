02c0d3b4e992da93b17befdaf391608a06f2163d
alexisjapas <alexis.japas.dev@pm.me>
2022-10-12 16:50:58 +0200

    Cleaned code

--- score.js
+++ score.js
@@ -1,22 +1,26 @@
+// Imports
 const express = require('express')
 const os = require('os')
 const bodyParser = require('body-parser')
 
-const app = express()
+// Port
 const port = process.env.PORT || 3001
 const host = 'localhost:3001'
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// App
+const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.text())
-var score = 0
+app.use((req, res, next) => {
+		res.header("Access-Control-Allow-Origin", "*")
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+		next()
+})
+
+// Score database management variables & functions
+var score
 var username
 var password
-app.use((req, res, next) => {
-	              res.header("Access-Control-Allow-Origin", "*");
-	              res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	              next();
-	  })
-
 app.get('/get_score', (req, res) => {
 	// Retrieves user id
 	//user = req.body.id
@@ -33,7 +37,7 @@ app.get('/get_score', (req, res) => {
 	res.send(result)
 })
 
-
+// Listening
 app.listen(port, () => {
 	console.log('Application running on ' + os.hostname() + " port : " + port)
 })
