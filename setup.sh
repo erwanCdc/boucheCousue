77ba0e89d3ccb368a998991ca7f036a6285626b6
alexisjapas <alexis.japas.dev@pm.me>
2022-10-13 14:50:56 +0200

    update.sh

--- setup.sh
+++ setup.sh
@@ -14,4 +14,18 @@ echo "#### AUTH DEPENDENCIES ####"
 cd ../auth
 npm install express os body-parser
 
-echo "Setup done. To run the project, run: sudo sh run.sh"
+echo "#### BUILDING AUTH ####"
+cd auth
+docker build -t auth:latest .
+ 
+echo "#### BUILDING MAIN ####"
+cd ../main
+docker build -t main:latest .
+ 
+echo "#### BUILDING DATABASE ####"
+cd ../db
+docker build -t db:latest .
+
+echo "Setup done."
+echo "To run the project, run: sudo docker-compose up"
+echo "To update images, run: sudo sh update.sh"
